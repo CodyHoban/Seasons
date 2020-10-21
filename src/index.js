191cf68f5ec92fcfaca4f4058a6524137518b6ac
Cody <hoban.j.cody@gmail.com>
2020-10-20 23:01:27 -0400

    Adding State

--- src/index.js
+++ src/index.js
@@ -2,9 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-const App = () => {
-    return <div>Hi There!</div>;
-};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // THIS IS THE ONLY TIME  we do direct assignment to this.state
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                //we called setState!!!
+                this.setState({ lat: position.coords.latitude })
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message })
+            }
+        );
+    }
+
+    // React says we have to defin render!!
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+        return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading!</div>
+    }
+}
 
 ReactDOM.render(
     <App />,
