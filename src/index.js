f9191add9c3ddb9686e40f5f1e84b5357cb8da84
Cody <hoban.j.cody@gmail.com>
2020-10-21 22:13:47 -0400

    refactoring constructor and componentDidMount

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom'
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: '' };
 
-        // THIS IS THE ONLY TIME  we do direct assignment to this.state
-        this.state = { lat: null, errorMessage: '' };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                //we called setState!!!
-                this.setState({ lat: position.coords.latitude })
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message })
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
 
