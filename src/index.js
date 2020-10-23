943ab9dca050760744b77fa7b49fc26b4b6d5a1b
Cody <hoban.j.cody@gmail.com>
2020-10-22 22:08:22 -0400

    factoring season app & css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
+
 
 
 class App extends React.Component {
@@ -12,17 +15,25 @@ class App extends React.Component {
         );
     }
 
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+            }
+    
+            if (!this.state.errorMessage && this.state.lat) {
+                return <SeasonDisplay lat={this.state.lat} />
+            }
+    
+            return <Spinner  message="Please accept location request" />;
+    }
+
     // React says we have to defin render!!
     render() {
-        if (this.state.errorMessage && !this.state.lat) {
-        return <div>Error: {this.state.errorMessage}</div>
-        }
-
-        if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
-        }
-
-        return <div>Loading!</div>
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
